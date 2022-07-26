51a1c4312eddfaf2caa5fe5b29e9091ef85efe52
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-07-26 20:32:18 +0900

    fix commands bug

--- app/server.go
+++ app/server.go
@@ -4,33 +4,13 @@ import (
 	"github.com/tidwall/redcon"
 )
 
-type Config struct {
-	Version       string `yaml:"version"`
-	UseCache      bool
-	UseDirtyWrite bool
-	Redis         struct {
-		Port     string      `yaml:"port"`
-		Bind     string      `yaml:"bind"`
-		Password interface{} `yaml:"password"`
-	} `yaml:"redis"`
-	S3 struct {
-		Version   string `yaml:"version"`
-		Bucket    string `yaml:"bucket"`
-		Region    string `yaml:"region"`
-		AccessKey string `yaml:"access_key"`
-		SecretKey string `yaml:"secret_key"`
-		Prefix    string `yaml:"prefix"`
-		Endpoint  string `yaml:"endpoint"`
-	} `yaml:"s3"`
-}
-
 type Server struct {
-	Config *Config
+	Config Config
 	S3     *S3
 	Redis  *Redis
 }
 
-func NewServer(config *Config, s3 *S3, redis *Redis) *Server {
+func NewServer(config Config, s3 *S3, redis *Redis) *Server {
 	return &Server{
 		Config: config,
 		S3:     s3,
@@ -39,7 +19,6 @@ func NewServer(config *Config, s3 *S3, redis *Redis) *Server {
 }
 
 func (s *Server) Run() error {
-	//net.parseAd
 	laddr := s.Config.Redis.Bind + ":" + s.Config.Redis.Port
 	err := redcon.ListenAndServe(laddr, s.Redis.handler, func(conn redcon.Conn) bool {
 		// Use this function to accept or deny the connection.
