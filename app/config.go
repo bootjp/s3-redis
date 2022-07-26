207679baccaeb494f2fb297872969be0f4b36aea
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-07-27 00:05:15 +0900

    support environ variable and refactor config load

--- app/config.go
+++ app/config.go
@@ -1,21 +1,21 @@
 package app
 
 type Config struct {
-	Version       string `yaml:"version"`
-	UseCache      bool
-	UseDirtyWrite bool
+	Version       string `mapstructure:"version"`
+	UseCache      bool   `mapstructure:"use_cache"`
+	UseDirtyWrite bool   `mapstructure:"use_dirty_write"`
 	Redis         struct {
-		Port     string      `yaml:"port"`
-		Bind     string      `yaml:"bind"`
-		Password interface{} `yaml:"password"`
-	} `yaml:"redis"`
+		Port     string      `mapstructure:"port"`
+		Bind     string      `mapstructure:"bind"`
+		Password interface{} `mapstructure:"password"`
+	} `mapstructure:"redis"`
 	S3 struct {
-		Version   string `yaml:"version"`
-		Bucket    string `yaml:"bucket"`
-		Region    string `yaml:"region"`
-		AccessKey string `yaml:"access_key"`
-		SecretKey string `yaml:"secret_key"`
-		Prefix    string `yaml:"prefix"`
-		Endpoint  string `yaml:"endpoint"`
-	} `yaml:"s3"`
+		Version   string `mapstructure:"version"`
+		Bucket    string `mapstructure:"bucket"`
+		Region    string `mapstructure:"region"`
+		AccessKey string `mapstructure:"access_key"`
+		SecretKey string `mapstructure:"secret_key"`
+		Prefix    string `mapstructure:"prefix"`
+		Endpoint  string `mapstructure:"endpoint"`
+	} `mapstructure:"s3"`
 }
